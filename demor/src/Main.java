61f85decee5e98dea5a4a921196ce63a0bafb694
Sridhar0128 <155973124+Sridhar0128@users.noreply.github.com>
2024-07-09 20:09:29 +0530

    Add files via upload

---
+++ demor/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=987;
+        int b=657;
+        String s=String.valueOf(a);
+        String d=Integer.toString(b);
+        System.out.println(s);
+        System.out.println(d);
+
+
+    }
+}
